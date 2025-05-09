1cb9a5cb1c124ec8265d4e67e7c1eb398cb9285a
marlontamo2 <marlontamo2@gmail.com>
2025-05-09 18:13:46 +0800

    initial commit

---
+++ EmployeeAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using EmployeeAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAPI.Controllers
+{
+    [Route("api/controller")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Employee> GetEmployeeName()
+        {
+            return new List<Employee>()
+            {
+                new Employee
+                {
+                    firstName = "Marlon",
+                    lastName = "Tamo",
+                    email = "marlontamo@gmail.com",
+                    phone = 09950850882
+                }
+
+            };
+        }
+    }
+}
---
+++ EmployeeAPI/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeAPI.Models
+{
+    public class Employee
+    {
+        public required string firstName {  get; set; }
+        public required string lastName { get; set; }
+        public required string email { get; set; }
+
+        public required long phone { get; set; }
+
+    }
+}
